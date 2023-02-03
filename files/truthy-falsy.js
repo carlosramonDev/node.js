3a7c2ed46bf86626e9bf41b53c28b6430eba2a1c
carlosramonDev <carlosramonDev@gmail.com>
2023-02-03 18:42:21 -0300

    truthy falsy

---
+++ files/truthy-falsy.js
@@ -0,0 +1,36 @@
+const x = 0;
+
+// Boolean
+const userLogin = true
+const accountPay = false
+
+//true or false
+//0 -> false
+//1 -> true
+
+console.log(0 == false)//returns true
+console.log("" == false)//return true
+console.log(1 == true)
+
+
+console.log("null and undefined")
+//Undefined
+// null -> empty or nothing
+
+let myVar;
+let varNull = null;
+
+console.log(myVar);
+console.log(varNull);
+
+console.log("When undefined is in its pure form")
+
+let number = 3;
+let text = "Alura";
+
+console.log(typeof number);
+console.log(typeof text );
+
+
+console.log(typeof  myVar);
+console.log(typeof varNull);
