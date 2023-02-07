19e5ebae2adec90f8cc634ebca2fd44f238a3353
carlosramonDev <carlosramonDev@gmail.com>
2023-02-07 14:02:25 -0300

    ternary operator

--- files/comparison.js
+++ files/comparison.js
@@ -1,4 +1,18 @@
-// == (comparação implicita)
+// == (implicit comparison)
 
 const number = 5;
-const text
+const text="5";
+
+console.log(number === text)
+
+//typeof
+console.log(typeof number)
+console.log(typeof text)
+
+// == only compare the value
+// === compare value and data type
+
+// explicit conversion
+
+Number()
+String()
---
+++ files/temario.js
@@ -0,0 +1,11 @@
+const minimumAge = 18;
+const clientAge = 16;
+
+/* if (clientAge >= minimumAge) {
+    console.log("beer");
+} else {
+    console.log("juice");
+} */
+
+            //condition               //true   //false
+console.log(clientAge >= minimumAge ? "beer" : "juice");
