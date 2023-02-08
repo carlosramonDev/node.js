dfd3486ff5440be43d6ea710b4aa54de4a55d79f
carlosramonDev <carlosramonDev@gmail.com>
2023-02-08 11:09:04 -0300

    template strings

---
+++ files/template-strings.js
@@ -0,0 +1,9 @@
+const name = "Ju";
+const age = 2021-1981;
+const cityOfNascimento = "São Paulo";
+
+// const presentation = "my name is " + name + ", my age is " + age + " and I was born in the city of " + cityOfNascimento;
+
+const presentation = `my name is ${name}, my age is ${age} and I was born in the city of ${cityOfNascimento}`;
+
+console.log(presentation)
